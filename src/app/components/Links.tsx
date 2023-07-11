7881d609018a334fa74dbe6920b27d03c203fde4
epicjoanna <joannameijardine@gmail.com>
2023-07-11 01:13:53 +0100

    added link icons to carousel

---
+++ src/app/components/Links.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import CodeIcon from '@mui/icons-material/Code';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+
+interface LinksProps {
+  githubLink: string;
+  openLink: string;
+}
+
+class Links extends React.Component<LinksProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey: any) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <span className="links">
+        <a href={this.props.githubLink}>
+          <CodeIcon style={{
+            fontSize: 40
+          }}/>
+        </a>
+        <a href={this.props.openLink}>
+          <OpenInNewRoundedIcon style={{
+            fontSize: 35
+          }} />
+        </a>
+      </span>
+    );
+  }
+}
+
+export default Links;
--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -1,109 +1,98 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import '../styles/Portfolio.css';
-
-
-type Project = {
-  title: string;
-  description: string;
-  tech: string;
-  repo: string;
-  demo: string;
-  image: string;
-}
+import Links from "./Links";
+import "../styles/Portfolio.css";
 
 class Portfolio extends React.Component {
-
-  constructor(props: {} | Readonly<{}>) {
+  constructor(props: any) {
     super(props);
     this.state = {
       expanded: true,
-      activeKey: "1"
+      activeKey: "1",
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
 
   handleSelect(eventKey: any) {
     this.setState({
-      activeKey: eventKey
+      activeKey: eventKey,
     });
   }
 
   render() {
+    const carouselProjects: Record<string, any> = {
+      "My Portfolio": {
+        title: "my-portfolio",
+        description: "my personal website: joannajardine.com",
+        repo: "https://github.com/epicjoanna/my-portfolio",
+        demo: "https://www.joannajardine.com",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg",
+      },
+      "Next.js Course": {
+        title: "nextjs-course",
+        description:
+          "my progress from the udemy course by Maximilian Schwarzmüller.",
+        repo: "https://github.com/epicjoanna/nextjs-course",
+        demo: "Demo",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg",
+      },
+      "Item three": {
+        title: "Item three",
+        description: "Description",
+        repo: "Repository",
+        demo: "Demo",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg",
+      },
+      "Item four": {
+        title: "Item four",
+        description: "Description",
+        repo: "Repository",
+        demo: "Demo",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg",
+      },
+    };
 
-    const carouselProjects: Record<string, Project> = {
-    "My Portfolio": {
-      title: "my-portfolio",
-      description: "my personal website: joannajardine.com",
-      tech: "react/next.js",
-      repo: "Repository",
-      demo: "Demo",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-    },
-    "Next.js Course": {
-      title: "nextjs-course",
-      description: "my progress from the udemy course by Maximilian Schwarzmüller.",
-      tech: "react/next.js",
-      repo: "Repository",
-      demo: "Demo",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-    },
-    "Item three": {
-      title: "Item three",
-      description: "Description",
-      tech: "Technologies",
-      repo: "Repository",
-      demo: "Demo",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-    },
-    "Item four": {
-      title: "Item four",
-      description: "Description",
-      tech: "Technologies",
-      repo: "Repository",
-      demo: "Demo",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-    },
-  };
+    return (
+      <section id="portfolio">
+        <h1 className="section">/portfolio</h1>
+        <div className="portfolio-content">
+          <p>
+            Check out more of my GitHub projects{" "}
+            <a href="https://github.com/epicjoanna?tab=repositories">here</a>!
+          </p>
 
-  return (
-    <section id="portfolio">
-      <h1 className="section">/portfolio</h1>
-      <div className="portfolio-content">
-      <p>Check out more of my GitHub projects <a href="https://github.com/epicjoanna?tab=repositories">here</a>!</p>
-
-      <div style={{ display: 'block', width: 1000 }}>
-      <Carousel>
-
-      {Object.keys(carouselProjects).map((key, i) => (
-        <Carousel.Item interval={ 1500 }>
-          <img
-            className="d-block w-100"
-            src={carouselProjects[key]["image"]}
-            alt={key}
-          />
-            <Carousel.Caption>
-            <h3>{carouselProjects[key]["title"]}</h3>
-    <p>
-        {carouselProjects[key]["description"]}
-        <br/>
-        {carouselProjects[key]["tech"]}
-    </p>
-    <p>
-        {carouselProjects[key]["repo"]}
-        <br/>
-        {carouselProjects[key]["demo"]}
-    </p>
-            </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-      </Carousel>
-    </div>
-    </div>
-    </section>
-  );
-  };
+          <div style={{ display: "block", width: 1000 }}>
+            <Carousel>
+              {Object.keys(carouselProjects).map((key, i) => (
+                <Carousel.Item interval={1500}>
+                  <img
+                    className="d-block w-100"
+                    src={carouselProjects[key]["image"]}
+                    alt={key}
+                  />
+                  <Carousel.Caption>
+                    <h3>{carouselProjects[key]["title"]}</h3>
+                    <p>
+                      {carouselProjects[key]["description"]}
+                      <br />
+                      <Links
+                        githubLink={carouselProjects[key]["repo"]}
+                        openLink={carouselProjects[key]["demo"]}
+                      ></Links>
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Portfolio;
