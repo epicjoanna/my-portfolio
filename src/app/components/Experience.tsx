a8e2e1e387edbb3eb8ad631666fb39ed170c42b1
epicjoanna <joannameijardine@gmail.com>
2023-07-12 23:58:35 +0100

    (attempted) a timeline under experience

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,16 +1,22 @@
-import * as React from 'react';
-import '../styles/Experience.css';
+import * as React from "react";
+import "../styles/Experience.css";
 
 function Experience() {
   return (
     <>
-    <section id="experience">
-      <h1 className="section">/experience</h1>
-      <div className="experience-content">
-      <p>
-        text
-      </p>
-      </div>
+      <section id="experience">
+        <h1 className="section">/experience</h1>
+        <div className="experience-content">
+          <div className="timeline">
+            <div className="container right">
+            <p>text
+              <br />
+              text
+            </p>
+            </div>
+            <span className="timeline_rounder" />
+          </div>
+        </div>
       </section>
     </>
   );
