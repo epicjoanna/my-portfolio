9bfeb87e4ba7fe518c5f6114e2a3cbc610a93dd4
epicjoanna <joannameijardine@gmail.com>
2023-07-13 23:52:04 +0100

    tidied up timeline code

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,40 +1,64 @@
 import * as React from "react";
 import "../styles/Experience.css";
 
-function Experience() {
-  return (
-    <>
-      {/* tidy this later */}
-      <section id="experience">
-        <h1 className="section">/experience</h1>
-        <div className="experience-content">
-          <div className="timeline">
-            <div className="container right">
-              <p>
-                <b>Graduate Apprentice Software Engineer</b>
-                <br />
-                <span className="date">October 2022 - Present</span>
-                <br />
-                <b>Smarter Grid Solutions</b>
-                <br />
-                Working as part of the UI modernisation team for Cirrus Flex,
-                using React/Next.js
-              </p>
-            </div>
-            <div className="container right">
-              <p>
-                <b>Software Engineering (Graduate Apprenticeship) BSc</b>
-                <br />
-                <span className="date">October 2022 - Present</span>
-                <br />
-                <b>University of Glasgow</b>
-              </p>
-            </div>
+class Experience extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(eventKey: any) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+
+  render() {
+    const prevExperience: Record<string, any> = {
+      currentJob: {
+        jobTitle: "Graduate Apprentice Software Engineer",
+        date: "October 2022 - Present",
+        company: "Smarter Grid Solutions",
+        description:
+          "Working as part of the UI modernisation team for Cirrus Flex, using React/Next.js",
+      },
+      education: {
+        jobTitle: "Software Engineering (Graduate Apprenticeship) BSc",
+        date: "October 2022 - Present",
+        company: "University of Glasgow",
+        description: "",
+      },
+    };
+
+    return (
+      <>
+        <section id="experience">
+          <h1 className="section">/experience</h1>
+          <div className="experience-content">
+            {Object.keys(prevExperience).map((key, i) => (
+              <div className="timeline">
+                <div className="container right">
+                  <p>
+                    <b>{prevExperience[key]["jobTitle"]}</b>
+                    <br />
+                    <span className="date">{prevExperience[key]["date"]}</span>
+                    <br />
+                    <b>{prevExperience[key]["company"]}</b>
+                    <br />
+                    {prevExperience[key]["description"]}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      </section>
-    </>
-  );
+        </section>
+      </>
+    );
+  }
 }
 
 export default Experience;
