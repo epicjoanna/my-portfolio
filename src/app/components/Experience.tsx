dbd494858d9ac89b3b40e2b98e6da55d4248eeb6
epicjoanna <joannameijardine@gmail.com>
2023-07-13 22:18:42 +0100

    improved timeline a lil'

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -4,17 +4,32 @@ import "../styles/Experience.css";
 function Experience() {
   return (
     <>
+      {/* tidy this later */}
       <section id="experience">
         <h1 className="section">/experience</h1>
         <div className="experience-content">
           <div className="timeline">
             <div className="container right">
-            <p>text
-              <br />
-              text
-            </p>
+              <p>
+                <b>Graduate Apprentice Software Engineer</b>
+                <br />
+                <span className="date">October 2022 - Present</span>
+                <br />
+                <b>Smarter Grid Solutions</b>
+                <br />
+                Working as part of the UI modernisation team for Cirrus Flex,
+                using React/Next.js
+              </p>
             </div>
-            <span className="timeline_rounder" />
+            <div className="container right">
+              <p>
+                <b>Software Engineering (Graduate Apprenticeship) BSc</b>
+                <br />
+                <span className="date">October 2022 - Present</span>
+                <br />
+                <b>University of Glasgow</b>
+              </p>
+            </div>
           </div>
         </div>
       </section>
