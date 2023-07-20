4b6a803de2b117d600b9e04ffd104127f046cb4d
epicjoanna <joannameijardine@gmail.com>
2023-07-20 22:47:01 +0100

    replaced record with array in experience section

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -3,60 +3,51 @@ import "../styles/Experience.css";
 import FadeIn from "./FadeIn";
 
 class Experience extends React.Component {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      expanded: true,
-      activeKey: "1",
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-
-  handleSelect(eventKey: any) {
-    this.setState({
-      activeKey: eventKey,
-    });
-  }
-
   render() {
-    const prevExperience: Record<string, any> = {
-      currentJob: {
+    const prevExperience = [
+      {
+        id: 1,
         jobTitle: "Graduate Apprentice Software Engineer",
         date: "October 2022 - Present",
         company: "Smarter Grid Solutions",
         description:
           "Working as part of the UI modernisation team for Cirrus Flex, using React/Next.js",
       },
-      education: {
+      {
+        id: 2,
         jobTitle: "Software Engineering (Graduate Apprenticeship) BSc",
         date: "October 2022 - Present",
         company: "University of Glasgow",
         description: "",
       },
-    };
+    ];
 
     return (
       <>
         <section id="experience">
-        <FadeIn>
-          <h1 className="section">/experience</h1>
-          <div className="experience-content">
-            {Object.keys(prevExperience).map((key, i) => (
-              <div className="timeline">
-                <div className="container right">
-                  <p>
-                    <b>{prevExperience[key]["jobTitle"]}</b>
-                    <br />
-                    <span className="date">{prevExperience[key]["date"]}</span>
-                    <br />
-                    <b>{prevExperience[key]["company"]}</b>
-                    <br />
-                    {prevExperience[key]["description"]}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+          <FadeIn>
+            <h1 className="section">/experience</h1>
+            <div className="experience-content">
+              {prevExperience.map(function (job) {
+                return (
+                  <div className="timeline">
+                    <div className="container right">
+                      <div key={job.id}>
+                        <p>
+                          <b>{job.jobTitle}</b>
+                          <br />
+                          <span className="date">{job.date}</span>
+                          <br />
+                          <b>{job.company}</b>
+                          <br />
+                          {job.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </FadeIn>
         </section>
       </>
