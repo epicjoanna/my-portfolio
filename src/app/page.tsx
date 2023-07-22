e1f24f8b96e08c4635885fd44d90267848346f89
epicjoanna <joannameijardine@gmail.com>
2023-07-22 01:47:48 +0100

    deleted unused fonts and added typographic features to inter font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,32 @@
-'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import SendRoundedIcon from '@mui/icons-material/SendRounded';
-import Stack from '@mui/material/Stack';
-import ReactDOM from 'react-dom/client';
-import About from './components/About';
-import Home from './components/Home';
-import Sidebar from './components/Sidebar';
-import Experience from './components/Experience';
-import Portfolio from './components/Portfolio';
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import Stack from "@mui/material/Stack";
+import ReactDOM from "react-dom/client";
+import About from "./components/About";
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
 import { BrowserRouter } from "react-router-dom";
 
 export default function Page() {
   return (
     <main>
-    <div className="sidebar">
-      <Sidebar />
-    </div>
-    <Home />
-    <div className="content">
-    <About />
-    <Experience />
-    <Portfolio />
-    </div>
+      <link rel="preconnect" href="https://rsms.me/" />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <Home />
+      <div className="content">
+        <About />
+        <Experience />
+        <Portfolio />
+      </div>
     </main>
-  )
+  );
 }
